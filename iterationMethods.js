036ed81f377744e6f63cd67a6c416efaf59a65f7
Hamad Almatouq <hamad.almatouq@outlook.com>
2024-11-07 00:38:57 +0300

    Better solution

--- iterationMethods.js
+++ iterationMethods.js
@@ -10,44 +10,41 @@ const transactions = [
 function filterIncomeTransactions(transactions) {
   // write your code here...
 
-  return transactions.filter(function (transactions) {
-    return transactions[0] === "income";
-  });
+  return transactions.filter((transactions) => transactions[0] === "income");
 }
 
 // 2) Write a `filterExpenseTransactions` function that return a new array containing only the expense transactions.
 function filterExpenseTransactions(transactions) {
   // write your code here...
-  // return transactions.filter(funcation(transactions) {
-  //   return transaction[0] === "expense";
-  // });
 
-  return transactions.filter(function (transactions) {
-    return transactions[0] === "expense";
-  });
+  return transactions.filter((transactions) => transactions[0] === "expense");
 }
 
 // 3) Write a `calculateTotalIncome` function that calculates the total income and returns it.
 function calculateTotalIncome(transactions) {
   // write your code here...
-  return transactions.reduce(function (total, transaction) {
-    if (transaction[0] === "income") {
-      return total + transaction[1];
-    }
-    return total;
-  }, 0);
+  // return transactions.reduce(function (total, transaction) {
+  //   if (transaction[0] === "income") {
+  //     return total + transaction[1];
+  //   }
+  //   return total;
+  // }, 0);
+
+  const incomeTransactions = filterIncomeTransactions(transactions);
+  return (
+    incomeTransactions.reduce((total, transaction) => total + transaction[1]), 0
+  );
 }
 
 // 4) Write a `calculateTotalExpenses` function that calculates the total expense and returns it.
 function calculateTotalExpenses(transactions) {
   // write your code here...
-  const expenseTransactions = transactions.filter(function (transaction) {
-    return transaction[0] === "expense";
-  });
+  const expenseTransactions = filterExpenseTransactions(transactions);
 
-  return expenseTransactions.reduce(function (total, transaction) {
-    return total + transaction[1];
-  }, 0);
+  return (
+    expenseTransactions.reduce((total, transaction) => total + transaction[1]),
+    0
+  );
 }
 
 // 5) Write a `calculateNetTotal` function that calculates the net total (total income - total expenses) and returns it.
